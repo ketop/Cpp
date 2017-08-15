d41345a72ed16f5e2c585bd86584fb5884e9446c
Patrick Dai <hahpklai@sina.com>
2017-08-15 15:37:50 +0800

    Update LeetCode.cpp
    find the longest  unrepeatable substring

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -1,5 +1,7 @@
 /*
-*  在string中找到最长无重复字符的子串。返回该子串长度  DP（O(n）)
+*  在string中找到最长无重复字符的子串。返回该子串长度  DP（O(n))
+*  使用dict来存储ASCII码中的字符在string中的索引。 如果出现重复字符，则更新计数起点，该起点为该字符之前在string中的索引。
+*  用此作差可以得到当前不重复字符子串的长度
 */
     int lengthOfLongestSubstring(string s) {
        vector<int> dict(256, -1);
